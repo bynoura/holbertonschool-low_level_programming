2871cb509350393c12ffe537d947ceb9c9e335c1
bynoura <nr.abdullah@gmail.com>
2025-08-25 22:25:58 +0000

    fix10

--- functions_nested_loops/10-main.c
+++ functions_nested_loops/10-main.c
@@ -1,32 +1,33 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * main - test add function and print using write
+ * main - test add function with multiple cases
  *
  * Return: Always 0
  */
 int main(void)
 {
-    print_number(add(89, 9));
-    write(1, "\n", 1);
+	print_number(add(89, 9));
+	write(1, "\n", 1);
 
-    print_number(add(0, 9));
-    write(1, "\n", 1);
+	print_number(add(0, 9));
+	write(1, "\n", 1);
 
-    print_number(add(0, 0));
-    write(1, "\n", 1);
+	print_number(add(0, 0));
+	write(1, "\n", 1);
 
-    print_number(add(89, 0));
-    write(1, "\n", 1);
+	print_number(add(89, 0));
+	write(1, "\n", 1);
 
-    print_number(add(-89, 12));
-    write(1, "\n", 1);
+	print_number(add(-89, 12));
+	write(1, "\n", 1);
 
-    print_number(add(12, 12));
-    write(1, "\n", 1);
+	print_number(add(12, 12));
+	write(1, "\n", 1);
 
-    print_number(add(-89, -98));
-    write(1, "\n", 1);
+	print_number(add(-89, -98));
+	write(1, "\n", 1);
 
-    return (0);
+	return (0);
 }
