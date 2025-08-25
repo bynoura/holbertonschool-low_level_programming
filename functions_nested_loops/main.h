ee513ed96ce2519f528f471217db64b46345b191
bynoura <nr.abdullah@gmail.com>
2025-08-25 19:42:35 +0000

    fix add7

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int); /* ← أضف هذا */
-
-#endif
+int print_last_digit(int);
