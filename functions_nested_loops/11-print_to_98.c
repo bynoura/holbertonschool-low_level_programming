8e40c7140a907db41ed070ef8ea831dcb2c77c05
bynoura <nr.abdullah@gmail.com>
2025-08-25 23:15:36 +0000

    task

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,33 @@
 #include <unistd.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
+ * print_number - prints an integer using write
+ * @n: the number to print
+ */
+void print_number(int n)
+{
+	char c;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		if (n <= -10)
+			print_number(-(n / 10));
+		c = '0' + -(n % 10);
+		write(1, &c, 1);
+	}
+	else
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		c = '0' + (n % 10);
+		write(1, &c, 1);
+	}
+}
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: starting number
  */
 void print_to_98(int n)
 {
