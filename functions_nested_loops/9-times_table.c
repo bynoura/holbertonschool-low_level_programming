e081f5c4763b920a9bfa1952b0e8b34759bc74e9
bynoura <nr.abdullah@gmail.com>
2025-08-25 21:49:43 +0000

    fix a0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,6 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			prod = i * j;
-
 			if (j == 0)
 			{
 				_putchar(prod + '0');
@@ -23,7 +22,6 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-
 				if (prod < 10)
 				{
 					_putchar(' ');
