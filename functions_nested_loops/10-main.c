c258f324b95c59eca69597947d6fb1820d46f25d
bynoura <nr.abdullah@gmail.com>
2025-08-25 22:07:46 +0000

    fix10

---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point to test the add function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int result;
+
+    result = add(89, 9);
+    printf("%d\n", result);
+
+    return (0);
+}
