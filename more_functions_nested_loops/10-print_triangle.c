750aaa862cd97cf525456d6675cade84ddf2c087
bynoura <nr.abdullah@gmail.com>
2025-09-02 13:48:29 +0300

    task10 fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,22 +1,31 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a right-aligned triangle using #
- * @size: The height of the triangle
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
 void print_triangle(int size)
 {
-int row, space, hash;
-if (size <= 0)
-{
-_putchar('\n');
-}
-for (row = 1; row <= size; row++)
-{
-for (space = size - row; space > 0; space--)
-_putchar(' ');
-for (hash = 1; hash <= row; hash++)
-_putchar('#');
-_putchar('\n');
-}
+    int row, space, hash;
+
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (row = 1; row <= size; row++)
+    {
+        /* print spaces */
+        for (space = size - row; space > 0; space--)
+        {
+            _putchar(' ');
+        }
+        /* print hashes */
+        for (hash = 1; hash <= row; hash++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
