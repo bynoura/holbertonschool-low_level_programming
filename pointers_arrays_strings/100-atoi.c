fc2b801a1f32f061641db63b1ea5a5e38417755d
Mlouk <mlouk_@Mlouks-MacBook-Air.local>
2025-09-05 11:19:57 +0300

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,15 +2,15 @@
 #include <limits.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Return: integer value of the string
+ * Return: The converted integer, or 0 if no numbers found
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, started = 0;
-	int res = 0;
+	int i = 0, sign = 1, num_started = 0;
+	int result = 0;
 
 	while (s[i] != '\0')
 	{
@@ -20,22 +20,26 @@ int _atoi(char *s)
 			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			int digit = s[i] - '0';
+			num_started = 1;
 
-			if (res > (INT_MAX - digit) / 10)
+			if (sign == -1)
 			{
-				if (sign == 1)
-					return (INT_MAX);
-				else
+				if (result < (INT_MIN + (s[i] - '0')) / 10)
 					return (INT_MIN);
+				result = result * 10 - (s[i] - '0');
 			}
-			res = res * 10 + digit;
-			started = 1;
+			else
+			{
+				if (result > (INT_MAX - (s[i] - '0')) / 10)
+					return (INT_MAX);
+				result = result * 10 + (s[i] - '0');
+			}
 		}
-		else if (started)
+		else if (num_started)
 			break;
+
 		i++;
 	}
 
-	return (res * sign);
+	return (result);
 }
