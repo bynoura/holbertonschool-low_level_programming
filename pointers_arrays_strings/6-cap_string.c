ad00b7a6a8262b117668849b64eb6434797881b8
Mlouk <mlouk_@Mlouks-MacBook-Air.local>
2025-09-05 11:48:23 +0300

    task66

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string
- * @str: Pointer to the string
+ * cap_string - capitalizes all words of a string
+ * @s: the input string
  *
- * Return: Pointer to the modified string
+ * Return: pointer to the modified string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i = 0, j;
-	char sep[] = " \t\n,;.!?\"(){}";
-
-	while (str[i] != '\0')
-	{
-		/* إذا كان أول حرف أو بعد separator */
-		if (i == 0 || (strchr(sep, str[i - 1]) != NULL))
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-			}
-		}
-		i++;
-	}
-
-	return (str);
+int i = 0;
+int j;
+char sep[] = " \t\n,;.!?\"(){}";
+while (s[i] != '\0')
+{
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+for (j = 0; sep[j] != '\0'; j++)
+{
+if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
+break;
+}
+}
+i++;
+}
+return (s);
 }
