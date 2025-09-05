00aab6f3eb59b49f5b1c2c72589d558fca9adc53
Mlouk <mlouk_@Mlouks-MacBook-Air.local>
2025-09-05 11:13:38 +0300

    task10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -8,30 +9,33 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	unsigned int result = 0;
-	int started = 0;
+	int i = 0, sign = 1, started = 0;
+	int res = 0;
 
 	while (s[i] != '\0')
 	{
-		/* عد الإشارات */
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			; /* نتجاهل */
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			int digit = s[i] - '0';
+
+			if (res > (INT_MAX - digit) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+			res = res * 10 + digit;
 			started = 1;
-			result = result * 10 + (s[i] - '0');
 		}
 		else if (started)
-		{
-			/* إذا بدأنا نقرأ أرقام وبعدين وقفنا */
 			break;
-		}
 		i++;
 	}
 
-	return (sign * (int)result);
+	return (res * sign);
 }
