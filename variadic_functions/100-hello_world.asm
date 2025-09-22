221914d222d4ff4c5fa2906fd168cc97cfc8ecf5
Mlouk <mlouk_@Mlouks-MacBook-Air.local>
2025-09-22 08:31:01 +0300

    task3

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,15 @@
+section .text
+	  global main
+main:
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, message
+	    mov rdx, message_end - message  ; NASM computes size
+	    syscall
+
+	    mov rax, 60
+	    xor rdi, rdi
+	    syscall
+	  section .data
+message:	 db "Hello, World", 0xA
+message_end:
