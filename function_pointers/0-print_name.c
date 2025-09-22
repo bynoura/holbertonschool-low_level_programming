8b51b7cc521a0ada967cc96497dccb35cf1081b9
Mlouk <mlouk_@Mlouks-MacBook-Air.local>
2025-09-22 08:18:53 +0300

    task1

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
+#include <stddef.h>        /* for NULL */
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name using a function pointer
- * @name: string to print
- * @f: function pointer to the printing function
+ * @name: name to be printed
+ * @f: pointer to a function that takes a string and returns nothing
  *
- * Return: Nothing
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
